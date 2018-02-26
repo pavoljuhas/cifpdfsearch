1e005adfea4247c2ecf91648d2289fe5dcb1e01b
Pavol Juhas <pavol.juhas@gmail.com>
2018-02-26 18:40:06 -0500

    Add utility functions jload, cfpath

--- src/ciflastic/__init__.py
+++ src/ciflastic/__init__.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
-__all__ = ['cifid', 'cifdocument']
+__all__ = ['cifid', 'cifdocument', 'jload', 'cfpath']
+
+import json
+import os.path
 
 from ciflastic.cifdocument import cifid, cifdocument
+
+
+def jload(filename):
+    '''Load JSON document from the specified file.
+    '''
+    with open(filename) as fp:
+        rv = json.load(fp)
+    return rv
+
+
+def cfpath(basename):
+    """Return absolute path to standard files included in ciflastic.
+    """
+    return os.path.join(_DATADIR, basename)
+
+
+_MYDIR = os.path.abspath(os.path.dirname(__file__))
+_DATADIR = os.path.normpath(os.path.join(_MYDIR, '../../standards'))
