2e751a0aa2ed8f0822998ea3a9432282c9bd35c7
Pavol Juhas <pavol.juhas@gmail.com>
2018-03-02 12:40:03 -0500

    Add `genjson` generator for streaming JSON entries
    Iterate over JSON entries without having to read the entire file.

--- src/ciflastic/_utils.py
+++ src/ciflastic/_utils.py
@@ -3,6 +3,9 @@
 """Internal helper functions.
 """
 
+import sys
+
+
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
@@ -37,3 +40,66 @@ def tofloat(s):
     else:
         rv = float(sbare)
     return rv
+
+
+def genjson(fp=None, filename=None, bufsize=262144, maxbufsize=None):
+    """Generate JSON entries from a file-like object.
+
+    Parameters
+    ----------
+    fp : file, optional
+        A file-like object with a sequence of JSON entries.
+    filename : str, optional
+        Path to a file with JSON entries.  The `filename` and
+        `fp` arguments are mutually exclusive; only one of
+        them must be provided.
+    bufsize : int, optional
+        Size of the buffer to be read from the file for decoding.
+        When decoding fails, the buffer is enlarged up to `maxbufsize`
+        or until the whole file is consumed.
+    maxbufsize : int, optional
+        Maximum allowed size of the internal read buffer.  When specified,
+        each JSON entry must be shorter than that.
+
+    Returns
+    -------
+    generator
+        The generator of JSON entries.
+
+    Raises
+    ------
+    JSONDecodeError
+        When JSON decoding fails or if some entry exceeds `maxbufsize`.
+    """
+    from json import JSONDecoder
+    if fp is None and filename is None:
+        raise TypeError("must have either 'fp' or 'filename' arguments")
+    if fp is not None and filename is not None:
+        raise TypeError("cannot use both 'fp' and 'filename' arguments")
+    if filename is not None:
+        kw = {'bufsize' : bufsize, 'maxbufsize' : maxbufsize}
+        with open(filename) as fp1:
+            for jobj in genjson(fp=fp1, **kw):
+                yield jobj
+        return
+    # here we have file-like object in fp
+    maxbuflen = sys.maxsize if maxbufsize is None else maxbufsize
+    decoder = JSONDecoder()
+    buf = ''
+    while True:
+        chunk = fp.read(bufsize)
+        buf += chunk
+        # end of file after processing last entry
+        if buf == '':
+            break
+        try:
+            jobj, i = decoder.raw_decode(buf)
+        except ValueError:
+            if chunk == '':
+                raise
+            if len(buf) > maxbuflen:
+                raise
+            continue
+        buf = buf[i:].lstrip()
+        yield jobj
+    return
