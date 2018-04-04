95234838cef4fe7acfeb35092819060d4406358e
Pavol Juhas <pavol.juhas@gmail.com>
2018-04-04 16:50:09 -0400

    ENH: script for calculating PDFs from CIF files
    Check for a near-zero peak in the result.  If it exists, process
    CIF with larger distance tolerance for near-symmetry positions.

--- src/ciflastic/apps/calcpdfs.py
+++ src/ciflastic/apps/calcpdfs.py
@@ -1,36 +1,73 @@
 #!/usr/bin/env python3
 
-'''Create single-entry for nickel CIF data.
+'''Calculate PDFs for the specified CIF files.
 '''
 
 import sys
 import os.path
+import argparse
 import numpy
-from ciflastic import config, cifpdf
-from pyobjcryst import loadCrystal
 
-pdfc = cifpdf.calculator.fromConfig(config.PDFCALCULATOR)
+parser = argparse.ArgumentParser(description=__doc__.strip())
+parser.add_argument('-c', '--config', metavar='FILE',
+                    help="Load the specified configuration file")
+parser.add_argument('-f', '--force', action='store_true',
+                    help="Overwrite existing result files.")
+parser.add_argument('output',
+                    help="Output directory for the npy files")
+parser.add_argument('cifs', nargs='+',
+                    help="CIF files for which to calculate PDFs. "
+                         "Replace '-' with files from standard input.")
 
-def main(ciflist):
-    with open(ciflist) as fp:
-        allcifs = [line.strip() for line in fp]
-    rncifs = [allcifs[i] for i in numpy.random.permutation(len(allcifs))]
-    for cf in rncifs:
-        fb = os.path.basename(cf)
-        nm = ''.join(c for c in fb if c.isdigit())
-        out = os.path.join(config.PDFSTORAGE, 'cod' + nm + '.npy')
-        if os.path.isfile(out):
+
+def calculate(pdfc, ciffile):
+    from pyobjcryst import loadCrystal
+    from diffpy.structure import loadStructure
+    lo = 0.5
+    crst = loadCrystal(ciffile)
+    r, g = pdfc(crst)
+    haszeropeak = lambda x: any(x[r < lo] > 0.01 * x.max())
+    if haszeropeak(g):
+        stru = loadStructure(ciffile, eps=0.001)
+        r, g = pdfc(stru)
+    if haszeropeak(g):
+        raise RuntimeError("contains near-zero peak")
+    return r, g
+
+
+def main(args):
+    from ciflastic import config, cifpdf, normcodid
+    from numpy.random import randint
+    from pyobjcryst import ObjCrystException
+    if args.config:
+        config.initialize(args.config)
+    if not os.path.isdir(args.output):
+        emsg = "{} must be a directory".format(args.output)
+        raise ValueError(emsg)
+    ciflist = args.cifs
+    if '-' in ciflist:
+        strippedlines = (line.strip() for line in sys.stdin)
+        extras = [a for a in strippedlines if a]
+        idx = ciflist.index('-')
+        ciflist = ciflist[:idx] + extras + ciflist[idx + 1:]
+    pdfc = cifpdf.calculator.fromConfig(config.PDFCALCULATOR)
+    while ciflist:
+        cf = ciflist.pop(randint(len(ciflist)))
+        codid = normcodid(cf)
+        out = os.path.join(args.output, 'cod{}.npy'.format(codid))
+        if not args.force and os.path.isfile(out):
             continue
         # create the file
         open(out, 'w').close()
         try:
-            crst = loadCrystal(cf)
-            r, g = pdfc(crst)
-        except ValueError:
+            r, g = calculate(pdfc, cf)
+        except Exception as e:
+            print('{}: {} {}'.format(cf, type(e).__name__, e))
             continue
         numpy.save(out, g.astype(numpy.float32))
     return
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    args = parser.parse_args()
+    main(args)
