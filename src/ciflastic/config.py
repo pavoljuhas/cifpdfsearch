7b5798957849069b03575904b564c364dfe1365c
Pavol Juhas <pavol.juhas@gmail.com>
2018-05-09 17:54:34 -0400

    ENH: use config in XDG_CONFIG_HOME/ciflastic/ciflastic.yml
    - Follow XDG standards for configuration file location.
    - Fall back to boilerplate if standard configuration cannot be found.
    - Add separate function for loading YAML configuration.

--- src/ciflastic/config.py
+++ src/ciflastic/config.py
@@ -13,27 +13,55 @@ CODDIR : str
     Absolute path to the Crystallography Open Database.
 """
 
+__all__ = ["CODDIR", "PDFSTORAGE", "PDFCALCULATOR", "loadConfig"]
+
+
+from warnings import warn
+from os.path import expanduser, abspath, join as pathjoin
+from xdg.BaseDirectory import load_first_config, xdg_config_dirs
+import yaml
+import ciflastic
+
+# Configuration constants ----------------------------------------------------
 
 CODDIR = ''
 PDFSTORAGE = ''
 PDFCALCULATOR = {}
 
 
-def initialize(cfgfile=None):
-    from warnings import warn
-    from os.path import isfile, expanduser, abspath
-    import yaml
-    from ciflastic import datapath
-    if cfgfile is None:
-        cfgfile = datapath('config/ciflastic.yml')
-        if not isfile(cfgfile):
-            wmsg = "Cannot open default configuration file {}".format(cfgfile)
-            warn(wmsg)
-            return
-    global CODDIR
-    global PDFSTORAGE
+def loadConfig(cfgfile):
+    """
+    Load dictionary of ciflastic configuration values.
+
+    Arguments
+    ---------
+    cfgfile : str
+        Path to configuration file in YAML format.
+
+    Returns
+    -------
+    cfg : dict
+        Dictionary of configuration values.
+    """
     with open(cfgfile) as fp:
         cfg = yaml.load(fp)
+    return cfg
+
+
+def initialize(cfgfile=None):
+    cfname = 'ciflastic/ciflastic.yml'
+    if cfgfile is None:
+        cfgfile = load_first_config(cfname)
+        if cfgfile is None:
+            cf = pathjoin(xdg_config_dirs[0], cfname)
+            tf = ciflastic.datapath('config/ciflastic.yml.template')
+            wmsg = ("Cannot find configuration file {}.\n"
+                    "Falling back to {}.").format(cf, tf)
+            warn(wmsg)
+            return
+    cfg = loadConfig(cfgfile)
+    global CODDIR
+    global PDFSTORAGE
     CODDIR = abspath(expanduser(cfg['coddir']))
     PDFSTORAGE = abspath(expanduser(cfg.get('pdfstorage', '')))
     PDFCALCULATOR.clear()
